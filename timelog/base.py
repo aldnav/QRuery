852b32bceebaca9572c3a54997db13a6d550fbff
Aldrin  Navarro <aldrinnavarro16@gmail.com>
2015-07-30 17:57:11 +0800

    Added method to read from image

--- timelog/base.py
+++ timelog/base.py
@@ -9,7 +9,7 @@ import requests
 
 # TODO: Accept image
 def read_record(image_url):
-    """ Reads a qr code image and returns the data if possible """
+    """ Reads a qr code image url and returns the data if possible """
     qr = qrtools.QR()
     r = requests.get(image_url, stream=True)
     if r.status_code == 200:
@@ -24,4 +24,17 @@ def read_record(image_url):
             return qr.data
     raise Exception('No image fetched.')
 
-read_record(image_url='http://i.imgur.com/fers0Gj.png')
+def read_record_from_image(image):
+    """ Reads a qr code image and returns the data if possible """
+    qr = qrtools.QR()
+    try:
+        qr.decode(image)
+    except Exception, e:
+        raise Exception('Can\'t decode image: ' + str(e))
+    return qr.data
+
+# read_record(image_url='http://i.imgur.com/fers0Gj.png')
+# import glob
+# os.chdir('../')
+# image_file = glob.glob('qr.png')[0]
+# print read_record_from_image(image=image_file)
